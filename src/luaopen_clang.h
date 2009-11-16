8a40a88cdc7baf6cb796745f000bc72760aa1e66
grrrwaaa <grrrwaaa@e598beea-d243-11de-9aee-2b7272db14c4>
2009-11-16 03:04:23 +0000

    added license comments

--- src/luaopen_clang.h
+++ src/luaopen_clang.h
@@ -1,7 +1,31 @@
 /*
-	Lua wrapper to LLVM-Clang
- */
+Luaclang is a module (loadable library) for the Lua programming language (www.lua.org) providing bindings to the LLVM/Clang APIs (www.llvm.org), in order to control LLVM/Clang from within Lua scripts.
+Luaclang is built against the LLVM 2.6 stable release, for Lua 5.1.
+Luaclang aims to closely follow the LLVM API, but making use of Lua idioms where possible. 
 
+Luaclang is released under the MIT open source license
+@see http://www.opensource.org/licenses/mit-license.php
+
+Copyright (c) 2009 Graham Wakefield
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 #ifndef INC_LUA_CLANG_H
 #define INC_LUA_CLANG_H
 
