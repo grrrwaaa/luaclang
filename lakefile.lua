d5379d14a1b6ffde46fb5559ebb4660b1fe964c0
grrrwaaa <grrrwaaa@gmail.com>
2012-01-25 17:22:27 -0800

    working on OSX again

--- lakefile.lua
+++ lakefile.lua
@@ -1,20 +1,25 @@
 -- Lake file for luaclang
+
+local needs = { "clang", "llvm" }
+
 if PLAT == "Darwin" then
+
 	LLVM_DIR = 'osx/llvm-3.0'
 	LLVM_LIBS = "LLVMJIT LLVMTarget LLVMScalarOpts LLVMSupport LLVMLinker LLVMCore LLVMMC LLVMAnalysis LLVMipa LLVMipo LLVMTransformUtils LLVMInstCombine LLVMExecutionEngine LLVMCodeGen LLVMX86CodeGen LLVMX86Utils LLVMX86AsmPrinter LLVMSelectionDAG LLVMX86Desc LLVMX86Info LLVMX86Disassembler LLVMX86AsmParser LLVMMCParser LLVMAsmPrinter"
 
 	CLANG_DIR = 'osx/llvm-3.0'
-	CLANG_LIBS = "clangBasic clangFrontend clangAST clangSerialization clangSema clangLex clangAnalysis clangCodeGen clangDriver clangParse"
+	CLANG_LIBS = "clangFrontend clangLex clangParse clangDriver clangCodeGen clangSema clangSerialization clangAnalysis clangAST clangBasic"
 
 elseif PLAT == "Linux" then
 	LLVM_DIR = 'linux/llvm-3.0'
 	LLVM_LIBS = "LLVMXCoreCodeGen LLVMTableGen LLVMSystemZCodeGen LLVMSparcCodeGen LLVMPTXCodeGen LLVMPowerPCCodeGen LLVMMSP430CodeGen LLVMMipsCodeGen LLVMMCJIT LLVMRuntimeDyld LLVMObject LLVMMCDisassembler LLVMXCoreDesc LLVMXCoreInfo LLVMSystemZDesc LLVMSystemZInfo LLVMSparcDesc LLVMSparcInfo LLVMPowerPCDesc LLVMPowerPCInfo LLVMPowerPCAsmPrinter LLVMPTXDesc LLVMPTXInfo LLVMPTXAsmPrinter LLVMMipsDesc LLVMMipsInfo LLVMMipsAsmPrinter LLVMMSP430Desc LLVMMSP430Info LLVMMSP430AsmPrinter LLVMMBlazeDisassembler LLVMMBlazeAsmParser LLVMMBlazeCodeGen LLVMMBlazeDesc LLVMMBlazeAsmPrinter LLVMMBlazeInfo LLVMLinker LLVMipo LLVMInterpreter LLVMInstrumentation LLVMJIT LLVMExecutionEngine LLVMDebugInfo LLVMCppBackend LLVMCppBackendInfo LLVMCellSPUCodeGen LLVMCellSPUDesc LLVMCellSPUInfo LLVMCBackend LLVMCBackendInfo LLVMBlackfinCodeGen LLVMBlackfinDesc LLVMBlackfinInfo LLVMBitWriter LLVMX86Disassembler LLVMX86AsmParser LLVMX86CodeGen LLVMX86Desc LLVMX86AsmPrinter LLVMX86Utils LLVMX86Info LLVMAsmParser LLVMARMDisassembler LLVMARMAsmParser LLVMARMCodeGen LLVMARMDesc LLVMARMAsmPrinter LLVMARMInfo LLVMArchive LLVMBitReader LLVMAlphaCodeGen LLVMSelectionDAG LLVMAsmPrinter LLVMMCParser LLVMCodeGen LLVMScalarOpts LLVMInstCombine LLVMTransformUtils LLVMipa LLVMAnalysis LLVMTarget LLVMCore LLVMAlphaDesc LLVMAlphaInfo LLVMMC LLVMSupport"
 
 	CLANG_DIR = 'linux/llvm-3.0'
-	CLANG_LIBS = "clangFrontend clangLex clangParse clangDriver clangCodeGen clangSema clangSerialization clangAnalysis clangAST clangBasic "
+	CLANG_LIBS = "clangFrontend clangLex clangParse clangDriver clangCodeGen clangSema clangSerialization clangAnalysis clangAST clangBasic"
 
+	needs[#needs+1] = "pthread"
 	PTHREAD_DIR = '/usr/'
-	PTHREAD_LIBS = 'pthread' --> please set!
+	PTHREAD_LIBS = 'pthread'
 
 else
 	print (PLAT, "not supported")
@@ -38,12 +43,7 @@ cpp.shared{
 	flags = "-fno-rtti -fomit-frame-pointer -fno-exceptions -fPIC -O3",
 	--flags = "-fno-rtti -fomit-frame-pointer -fno-exceptions -fPIC -O3 -Woverloaded-virtual -Wcast-qual",
 	-- library dependencies:
-	needs = { 	
-		'clang',
-		'llvm',
-		'pthread',	-- Linux only?
-		'lua', 
-	},
+	needs = needs,
 	-- put results in debug / release folders 
 	--odir = true,	
 }
