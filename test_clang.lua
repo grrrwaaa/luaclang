5dd2fff9673244c0474e9ef8b778b8d1e1b77d7e
graham wakefield <grrrwaaa@gmail.com>
2012-01-25 17:21:02 -0800

    added missing test file

---
+++ test_clang.lua
@@ -0,0 +1,62 @@
+
+-- quick'n'dirty way to get current path:
+function cmd(str)
+	local f = assert(io.popen(str, "r"))
+	local res = f:read()
+	f:close()
+	return res and res:gsub("%c", "")	-- remove control characters
+end
+
+local separator = package.config:sub( 1, 1 )
+local pwd = assert(cmd"pwd") .. separator
+local script = pwd .. (arg and arg[0] or "")
+local path = {separator}
+for w in string.gmatch(script, string.format("[^%s]+%s", separator, separator)) do
+	table.insert(path, w)
+end
+path = table.concat(path)
+print(path)
+
+require "clang"
+	for k, v in pairs(clang) do print(string.format("clang.%s = %s", k, tostring(v))) end
+
+local cc = clang.Compiler()
+
+--local app = (require"app")
+cc:include(path .. "headers")
+
+cc:define("SCALAR=7")
+
+assert(cc:compile([=[
+
+extern "C" {
+	#include "lua.h"
+	#include "lauxlib.h"
+}
+
+extern "C" int test(lua_State * L) {
+	double x = luaL_optnumber(L, 1, 0);
+	lua_pushnumber(L, x*5);
+	return 1;
+}
+
+]=]))
+
+--cc:dump()
+cc:optimize()
+cc:dump()
+
+print(cc:datalayout())
+print(cc:targettriple())
+print(clang.lua_pointer())
+
+local jit = cc:jit()
+
+print(jit:getfunctionptr("test"))
+local f = assert(jit:pushcfunction("test"), "failed to find function")
+
+for i = 1, 10 do
+	print(i, f(i))
+end
+
+
